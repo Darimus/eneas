df1992955f6c02ef05c735616ca60c5f336c5fa5
Bruno Passos <bruno.passos@outlook.com.br>
2021-06-08 16:37:15 -0300

    :smile: Pensando na solucao de social midia

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+const btn_midia = document.getElementById('btn-midia');
+
+addEventListener('click', () => {
+
+    if(btn_midia.classList.contains('midia-on')) {
+        btn_midia.classList.remove('midia-on')
+    } else { 
+        btn_midia.classList.add('midia-on');
+    } 
+});
